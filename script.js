3ca1bae6dde9d00a621fe1ee8922ce78ba6d9314
muhammadhashim535 <sm395562@gmail.com>
2024-10-22 00:08:59 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+document.getElementById("name").addEventListener("click", function() {
+    const newRollNumber = prompt("Please enter your roll number:");
+    if (newRollNumber) {
+        document.getElementById("roll-number").innerText = newRollNumber;
+    }
+});
+
 document.getElementById("change-roll-button").addEventListener("click", function() {
     const newRollNumber = prompt("Please enter your roll number:");
     if (newRollNumber) {
